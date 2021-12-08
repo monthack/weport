0e03ff301042abfb5d66cd8c19d39127f8f9e08e
Montse Manrique <montsevm.03@gmail.com>
2021-12-08 12:07:23 -0600

    Revert "configuration for development"
    This reverts commit 0ddb2b901857a5f8ebcb5721b5bb8354734cae7e.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,1 @@
 require('./bootstrap');
-
-
-import { createApp } from 'vue';
-
-const app = createApp({
-    components:{
-       ExampleComponent: () => import('./components/ExampleComponent.vue') 
-    }
-});
