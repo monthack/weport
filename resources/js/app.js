0ddb2b901857a5f8ebcb5721b5bb8354734cae7e
Montse Manrique <montsevm.03@gmail.com>
2021-12-08 11:49:49 -0600

    configuration for development

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+
+
+import { createApp } from 'vue';
+
+const app = createApp({
+    components:{
+       ExampleComponent: () => import('./components/ExampleComponent.vue') 
+    }
+});
