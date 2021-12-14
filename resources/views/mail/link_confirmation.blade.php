84b74f3b379ece89340b86ca5ba3102bef29dad2
Montse Manrique <montsevm.03@gmail.com>
2021-12-14 09:52:19 -0600

    style the email

--- resources/views/mail/link_confirmation.blade.php
+++ resources/views/mail/link_confirmation.blade.php
@@ -1,13 +1,132 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hola {{$user_name}} </h1>
-    <p>El usuario {{$employed_name}} ha verificado su correo de manera exitosa! </p>
-</body>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="es">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Notificación de usuarios verificados</title>
+         <link rel="preconnect" href="https://fonts.gstatic.com">
+         <link href="https://fonts.googleapis.com/css2?family=Roboto&amp;display=swap" rel="stylesheet">
+        <style type="text/css">
+            body, table, td, a { -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; }
+            table, td { mso-table-lspace: 0pt; mso-table-rspace: 0pt; }
+            img { -ms-interpolation-mode: bicubic; }
+            /* RESET STYLES */
+            img { border: 0; outline: none; text-decoration: none; }
+            table { border-collapse: collapse !important; }
+            body { margin: 0 !important; padding: 0 !important; width: 100% !important; }
+            /* iOS BLUE LINKS */
+            a[x-apple-data-detectors] {
+            color: inherit !important;
+            text-decoration: none !important;
+            font-size: inherit !important;
+            font-family: inherit !important;
+            font-weight: inherit !important;
+            line-height: inherit !important;
+            }
+            /* ANDROID CENTER FIX */
+            div[style*="margin: 16px 0;"] { margin: 0 !important; }
+            /* MEDIA QUERIES */
+            @media  all and (max-width:639px){
+            .wrapper{ width:320px!important; padding: 0 !important; }
+            .container{ width:300px!important;  padding: 0 !important; }
+            .mobile{ width:300px!important; display:block!important; padding: 0 !important; }
+            .img{ width:100% !important; height:auto !important; }
+            *[class="mobileOff"] { width: 0px !important; display: none !important; }
+            *[class*="mobileOn"] { display: block !important; max-height:none !important; }
+            }
+        </style>
+    </head>
+    <body style="margin:0; padding:0; background-color:#F2F2F2;">
+        <span style="display: block; width: 640px !important; max-width: 640px; height: 1px" class="mobileOff"></span>
+        <center>
+            
+            <table width="100%" border="0" cellpadding="0" cellspacing="0" bgcolor="#F2F2F2">
+                <tr>
+                    <td align="center" valign="top">
+                        <table width="640" cellpadding="0" cellspacing="0" border="0" class="wrapper" bgcolor="#FFFFFF">
+                            <tr>
+                                <td align="center" valign="top">
+                                    <table width="640" cellpadding="0" cellspacing="0" border="0" class="wrapper">
+                                        <tr>
+                                            <td align="center" valign="top">
+                                                <img src="https://cdn.pixabay.com/photo/2017/05/31/23/44/email-marketing-2362038_960_720.png" width="600" height="154" alt="Imagen de fondo" class="img" style="width: 100%;">
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                        </table>
+
+
+                        <table width="640" cellpadding="0" cellspacing="0" border="0" class="wrapper" bgcolor="#FFFFFF">
+                            <tr>
+                                <td height="30" style="font-size:10px; line-height:10px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td align="center" valign="top">
+                                    <table width="500" cellpadding="0" cellspacing="0" border="0" class="container">
+                                        <tr>
+                                            <td align="left" valign="top">
+                                                <font size="5" color="#496281" face="Arial" style="font-family: 'Roboto', 'Arial', 'sans-serif';"><b style="font-weight: bold;">Estimado(a) {{$user_name}}</b></font>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td height="20" style="font-size:10px; line-height:20px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td align="center" valign="top">
+                                    <table width="500" cellpadding="0" cellspacing="0" border="0" class="container">
+                                        <tr>
+                                            <td align="left" valign="top">
+                                                <p>
+                                                    <font size="3" color="#333333" face="Arial" style="font-family: 'Roboto', 'Arial', 'sans-serif';">El usuario <b style="font-weight: bold">{{$employed_name}}</b> ha verificado su correo de manera exitosa!.</font>
+                                                </p>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td height="30" style="font-size:10px; line-height:30px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td height="30" style="font-size:10px; line-height:30px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td height="30" style="font-size:10px; line-height:30px;">&nbsp;</td>
+                            </tr>
+                        </table>
+                        <table width="640" cellpadding="0" cellspacing="0" border="0" class="wrapper" bgcolor="#89CDEF">
+                            <tr>
+                                <td height="30" style="font-size:10px; line-height:10px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td align="center" valign="top">
+                                    <table width="600" cellpadding="0" cellspacing="0" border="0" class="container">
+                                        <tr>
+                                            <td width="400" class="mobile" align="left" valign="top">
+                                                <font size="2" color="#ffffff" face="Arial" style="font-family: 'Roboto', 'Arial', 'sans-serif';">Montserrat Vazque Manrique♡ © 2021. Todos los derechos reservados. </font>
+                                                <br><br>
+                                                
+                                            </td>
+
+                                        </tr>
+                                    </table>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td height="30" style="font-size:10px; line-height:10px;">&nbsp;</td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </center>
+    </body>
 </html>
+
+
